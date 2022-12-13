c181f6f201959d5b1afa91c681cecfd5433cde40
Omar <mourra24@gmail.com>
2022-12-13 18:19:23 +0200

    testing the threejs library
    made sure that the parcel is working and that the threejs is properly installed

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+console.log(THREE);
+const scene= new THREE.Scene();
+const camera= new THREE.OrthographicCamera();
+const light=new THREE.AmbientLight();
+console.log("ahmed");
